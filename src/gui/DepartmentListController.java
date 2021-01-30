b7e289c07df7e14a6e899d486e7e3d6857f201df
EduHC <eduardo.cic.horn@gmail.com>
2021-01-30 14:34:25 -0300

    Saving new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -88,6 +88,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(department);
+			controller.setDepartmentSerice(new DepartmentService());
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
